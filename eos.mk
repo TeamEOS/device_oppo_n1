6ceb6ee0c989343e01683f6b3ad75de0a730ded8
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-29 11:28:09 +0100

    n1: EOS-ify

--- eos.mk
+++ eos.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product-if-exists, vendor/eos/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/oppo/n1/full_n1.mk)
 
-PRODUCT_NAME := cm_n1
+PRODUCT_NAME := eos_n1
 PRODUCT_DEVICE := n1
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
