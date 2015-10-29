571b72461219b7268427c12a751c019826cc735a
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-29 11:24:03 +0100

    n1: Remove non-AOSP scripts

--- README.md
+++
@@ -1,4 +0,0 @@
-Device tree for Oppo N1 (CyanogenMod Edition!)
-
-Copyright 2013, The CyanogenMod Project
-
--- extract-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export DEVICE=n1
-export VENDOR=oppo
-./../apq8064-common/extract-files.sh $@
